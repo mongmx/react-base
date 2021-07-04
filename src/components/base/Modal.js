54bab195bb5134d09fcc21e27bf32fbbd9b5aed0
Mong MX <mongmx@gmail.com>
2021-07-05 02:16:48 +0700

    Add Base and Modal component

---
+++ src/components/base/Modal.js
@@ -0,0 +1,61 @@
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+function useOnClickOutside(ref, handler) {
+    useEffect(() => {
+        const listener = event => {
+            if (!ref.current || ref.current.contains(event.target)) {
+                return;
+            }
+            handler(event);
+        };
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+        return () => {
+            document.removeEventListener("mousedown", listener);
+            document.removeEventListener("touchstart", listener);
+        };
+    });
+}
+
+const ModalBackground = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+`
+
+const ModalContainer = styled.div`
+    position: fixed;
+    background: white;
+    width: 600px;
+    height: auto;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 0 2px 4px 0 rgba(50, 50, 93, 0.1);
+    color: black;
+`
+
+function Modal({ showModal, setShowModal, children }) {
+    const ref = useRef();
+    useOnClickOutside(ref, () => setShowModal(false));
+    return (
+        <div>
+            {showModal && (
+                <ModalBackground>
+                    <ModalContainer ref={ref}>
+                        {children}
+                    </ModalContainer>
+                </ModalBackground>
+            )}
+        </div>
+    );
+}
+
+export default Modal;
---
+++ src/components/base/base.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+    border: 1px solid black;
+    height: 100vh;
+    margin-top: 20px;
+`;
+
+// export const Column = styled`
+
+// `;
+
+// export const Row = styled`
+
+// `;
